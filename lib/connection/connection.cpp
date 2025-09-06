1b7c1eaa362a6bb783826abfd728cbee521e9b3b
Louis <lourou99@gmail.com>
2025-09-06 14:16:33 +0200

    Added loop method that handles disconnects

--- lib/connection/connection.cpp
+++ lib/connection/connection.cpp
@@ -1,8 +1,10 @@
 #include <WiFi.h>
 #include "connection.h"
 
-Connection::Connection(const char *ssid, const char *password, const char *hostname)
-    : _ssid(ssid), _password(password), _hostname(hostname) {}
+#define LED_BUILTIN 8
+
+Connection::Connection(const char *ssid, const char *password, const char *hostname, Lighting *lights)
+    : _ssid(ssid), _password(password), _hostname(hostname), _lights(lights) {}
 
 void Connection::begin()
 {
@@ -10,12 +12,14 @@ void Connection::begin()
     WiFi.setHostname(_hostname);
     WiFi.begin(_ssid, _password);
 
-    Serial.print("Connecting to WiFi");
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    Serial.println("Connecting to WiFi");
     while (WiFi.status() != WL_CONNECTED)
     {
         if (_lights)
         {
-            _lights->blink(1, 200); // blink LED while waiting
+            _lights->blink(1, 50); // blink LED while waiting
         }
         else
         {
@@ -30,4 +34,15 @@ void Connection::printMac()
 {
     Serial.print("ESP32 MAC Address: ");
     Serial.println(WiFi.macAddress());
+}
+
+void Connection::loop()
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.println("WiFi disconnected, retrying...");
+        WiFi.begin(_ssid, _password);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(10000); // wait 10s before retry
+    }
 }
